91cfc555334d4bd3c35fddc06ec6341ce461fca6
Mahtasim Sami <gosamigo12@gmail.com>
2020-07-17 04:10:44 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,18 @@
+// feetToMile
+function feetToMile(feet){
+    var Mile=feet/5280;
+    return Mile;
+  }
+  var Mirpur=feetToMile(15840);
+  console.log(Mirpur);
+  var Khilgaon=feetToMile(2607)
+  console.log(Khilgaon);
+  var Gazipur=feetToMile(1704);
+  console.log(Gazipur);
+// tinyFriend
+
+
+  
+  
+  
+  
